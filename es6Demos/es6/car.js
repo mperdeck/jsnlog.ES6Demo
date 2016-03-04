bb2d7a7cf2011ac1b2f6ce34081908253723ebe8
mperdeck <mperdeck@gmail.com>
2016-03-05 01:21:17 +1100

    Initial ES6 conversion

---
+++ es6Demos/es6/car.js
@@ -0,0 +1,8 @@
+class Car {
+    
+    constructor(engine) {
+        this.engine = engine;
+    }
+
+}
+
---
+++ es6Demos/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    plumber = require('gulp-plumber'),
+    es6Path = 'es6/*.js',
+    compilePath = 'es6/compiled';
+
+gulp.task('babel', function () {
+    gulp.src([es6Path])
+        .pipe(plumber())
+        .pipe(babel())
+        .pipe(gulp.dest(compilePath + '/babel'));
+});
+
+gulp.task('default', ['babel']);
+
+
